69b52577761fd0836785d45a760083325a96f2e4
Fiery-Mouwnies <166019254+Fiery-Mouwnies@users.noreply.github.com>
2025-09-08 00:30:05 +0200

    Added package-lock.json

--- components/post-processing.tsx
+++ components/post-processing.tsx
@@ -2,11 +2,8 @@
 
 import { useFrame, useThree } from '@react-three/fiber';
 import { useMemo } from 'react';
-import { BloomNode } from 'three/examples/jsm/nodes/display/BloomNode.js';
-import { pass } from 'three/examples/jsm/nodes/Nodes.js';
-import { WebGPUPostProcessing } from 'three/examples/jsm/renderers/webgpu/WebGPUPostProcessing.js';
-
-import * as THREE from 'three';
+import { EffectComposer } from '@react-three/postprocessing';
+import { Bloom } from '@react-three/postprocessing';
 
 export const PostProcessing = ({
   strength = 1,
@@ -15,24 +12,13 @@ export const PostProcessing = ({
   strength?: number;
   threshold?: number;
 }) => {
-  const { gl, scene, camera } = useThree();
-
-  const render = useMemo(() => {
-    const postProcessing = new WebGPUPostProcessing(gl as any);
-    const scenePass = pass(scene, camera);
-    const scenePassColor = scenePass.getTextureNode('output');
-    const bloomPass = new BloomNode(scenePassColor, strength, 0.5, threshold);
-
-    const final = scenePassColor.add(bloomPass);
-
-    postProcessing.outputNode = final;
-
-    return postProcessing;
-  }, [camera, gl, scene, strength, threshold]);
-
-  useFrame(() => {
-    render.renderAsync();
-  }, 1);
-
-  return null;
+  return (
+    <EffectComposer>
+      <Bloom 
+        intensity={strength}
+        luminanceThreshold={threshold}
+        luminanceSmoothing={0.5}
+      />
+    </EffectComposer>
+  );
 };
